e9effc18f292ac6914eb639a51c94e50572b318d
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-07-30 23:55:51 -0700

    can compile a file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use std::{env, fs::File, io::Read};
+
+use ra::{codegen::codegen, parser::parse, tokenizer::tokenize, writer::write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("args = {:?}", args);
+    let mut file = File::open(&args[1]).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+    let tokens = tokenize(&contents);
+    let ast = parse(tokens);
+    let wasm = codegen(ast);
+    let file = File::create("start.wasm").unwrap();
+    write(file, wasm).unwrap();
 }
