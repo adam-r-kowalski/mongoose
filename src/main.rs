715bbd8687244c7ab85c69e8660e5d3b914f2ff6
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-07-31 00:04:32 -0700

    remove print in main

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,6 @@ use ra::{codegen::codegen, parser::parse, tokenizer::tokenize, writer::write};
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    println!("args = {:?}", args);
     let mut file = File::open(&args[1]).unwrap();
     let mut contents = String::new();
     file.read_to_string(&mut contents).unwrap();
