5f1d88f9b699f994205984578f509a2b2f3bebd8
Adam Kowalski <adam.kowalski.work@gmail.com>
2021-08-15 21:56:46 -0700

    string representation of strings

--- tests/test_tokenizer.rs
+++ tests/test_tokenizer.rs
@@ -3,132 +3,155 @@ use pretty_assertions::assert_eq;
 use smith::tokenizer::{tokenize, Kind, Tokens, TopLevel};
 use test_utilities::strings;
 
+fn token_string_literal(
+    top_level: &TopLevel,
+    token: usize,
+    mut output: String,
+    text: &str,
+) -> String {
+    output.push_str(text);
+    output.push_str("\n");
+    token_string_impl(top_level, token + 1, output)
+}
+
+fn token_string_symbol(top_level: &TopLevel, token: usize, mut output: String) -> String {
+    let text = &top_level.symbols[top_level.indices[token]];
+    output.push_str("Symbol ");
+    output.push_str(text);
+    output.push_str("\n");
+    token_string_impl(top_level, token + 1, output)
+}
+
+fn token_string_int(top_level: &TopLevel, token: usize, mut output: String) -> String {
+    let text = &top_level.ints[top_level.indices[token]];
+    output.push_str("Int ");
+    output.push_str(text);
+    output.push_str("\n");
+    token_string_impl(top_level, token + 1, output)
+}
+
+fn token_string_indent(top_level: &TopLevel, token: usize, mut output: String) -> String {
+    let indent = &top_level.indents[top_level.indices[token]];
+    output.push_str("Indent ");
+    output.push_str(&indent.to_string());
+    output.push_str("\n");
+    token_string_impl(top_level, token + 1, output)
+}
+
+fn token_string_impl(top_level: &TopLevel, token: usize, output: String) -> String {
+    match top_level.kinds.get(token) {
+        Some(Kind::Def) => token_string_literal(top_level, token, output, "Def"),
+        Some(Kind::LeftParen) => token_string_literal(top_level, token, output, "LeftParen"),
+        Some(Kind::RightParen) => token_string_literal(top_level, token, output, "RightParen"),
+        Some(Kind::Colon) => token_string_literal(top_level, token, output, "Colon"),
+        Some(Kind::Plus) => token_string_literal(top_level, token, output, "Plus"),
+        Some(Kind::Minus) => token_string_literal(top_level, token, output, "Minus"),
+        Some(Kind::Times) => token_string_literal(top_level, token, output, "Times"),
+        Some(Kind::Slash) => token_string_literal(top_level, token, output, "Slash"),
+        Some(Kind::Equal) => token_string_literal(top_level, token, output, "Equal"),
+        Some(Kind::Symbol) => token_string_symbol(top_level, token, output),
+        Some(Kind::Int) => token_string_int(top_level, token, output),
+        Some(Kind::Indent) => token_string_indent(top_level, token, output),
+        Some(kind) => panic!("not implemented for kind {:?}", kind),
+        None => output,
+    }
+}
+
+fn token_string(tokens: &Tokens) -> String {
+    tokens
+        .top_level
+        .iter()
+        .fold(String::from("\n"), |output, top_level| {
+            token_string_impl(top_level, 0, output)
+        })
+}
+
 #[test]
 fn test_tokenize_int() {
     let tokens = tokenize("def start(): 0");
     assert_eq!(
-        tokens,
-        Tokens {
-            top_level: vec![TopLevel {
-                indices: vec![0, 0, 0, 0, 0, 0],
-                kinds: vec![
-                    Kind::Def,
-                    Kind::Symbol,
-                    Kind::LeftParen,
-                    Kind::RightParen,
-                    Kind::Colon,
-                    Kind::Int
-                ],
-                symbols: strings(["start"]),
-                ints: strings(["0"]),
-                indents: vec![],
-            }]
-        }
-    )
+        token_string(&tokens),
+        r#"
+Def
+Symbol start
+LeftParen
+RightParen
+Colon
+Int 0
+"#
+    );
 }
 
 #[test]
 fn test_tokenize_add() {
     let tokens = tokenize("def start(): 5 + 10");
     assert_eq!(
-        tokens,
-        Tokens {
-            top_level: vec![TopLevel {
-                indices: vec![0, 0, 0, 0, 0, 0, 0, 1],
-                kinds: vec![
-                    Kind::Def,
-                    Kind::Symbol,
-                    Kind::LeftParen,
-                    Kind::RightParen,
-                    Kind::Colon,
-                    Kind::Int,
-                    Kind::Plus,
-                    Kind::Int
-                ],
-                symbols: strings(["start"]),
-                ints: strings(["5", "10"]),
-                indents: vec![],
-            }]
-        }
-    )
+        token_string(&tokens),
+        r#"
+Def
+Symbol start
+LeftParen
+RightParen
+Colon
+Int 5
+Plus
+Int 10
+"#
+    );
 }
 
 #[test]
 fn test_tokenize_subtract() {
     let tokens = tokenize("def start(): 5 - 10");
     assert_eq!(
-        tokens,
-        Tokens {
-            top_level: vec![TopLevel {
-                indices: vec![0, 0, 0, 0, 0, 0, 0, 1],
-                kinds: vec![
-                    Kind::Def,
-                    Kind::Symbol,
-                    Kind::LeftParen,
-                    Kind::RightParen,
-                    Kind::Colon,
-                    Kind::Int,
-                    Kind::Minus,
-                    Kind::Int
-                ],
-                symbols: strings(["start"]),
-                ints: strings(["5", "10"]),
-                indents: vec![],
-            }]
-        }
-    )
+        token_string(&tokens),
+        r#"
+Def
+Symbol start
+LeftParen
+RightParen
+Colon
+Int 5
+Minus
+Int 10
+"#
+    );
 }
 
 #[test]
 fn test_tokenize_multiply() {
     let tokens = tokenize("def start(): 5 * 10");
     assert_eq!(
-        tokens,
-        Tokens {
-            top_level: vec![TopLevel {
-                indices: vec![0, 0, 0, 0, 0, 0, 0, 1],
-                kinds: vec![
-                    Kind::Def,
-                    Kind::Symbol,
-                    Kind::LeftParen,
-                    Kind::RightParen,
-                    Kind::Colon,
-                    Kind::Int,
-                    Kind::Times,
-                    Kind::Int
-                ],
-                symbols: strings(["start"]),
-                ints: strings(["5", "10"]),
-                indents: vec![],
-            }]
-        }
-    )
+        token_string(&tokens),
+        r#"
+Def
+Symbol start
+LeftParen
+RightParen
+Colon
+Int 5
+Times
+Int 10
+"#
+    );
 }
 
 #[test]
 fn test_tokenize_divide() {
     let tokens = tokenize("def start(): 10 / 5");
     assert_eq!(
-        tokens,
-        Tokens {
-            top_level: vec![TopLevel {
-                indices: vec![0, 0, 0, 0, 0, 0, 0, 1],
-                kinds: vec![
-                    Kind::Def,
-                    Kind::Symbol,
-                    Kind::LeftParen,
-                    Kind::RightParen,
-                    Kind::Colon,
-                    Kind::Int,
-                    Kind::Slash,
-                    Kind::Int
-                ],
-                symbols: strings(["start"]),
-                ints: strings(["10", "5"]),
-                indents: vec![],
-            }]
-        }
-    )
+        token_string(&tokens),
+        r#"
+Def
+Symbol start
+LeftParen
+RightParen
+Colon
+Int 10
+Slash
+Int 5
+"#
+    );
 }
 
 #[test]
@@ -140,35 +163,27 @@ def start():
     x + y"#;
     let tokens = tokenize(source);
     assert_eq!(
-        tokens,
-        Tokens {
-            top_level: vec![TopLevel {
-                indices: vec![0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 2, 0, 1, 2, 3, 0, 4],
-                kinds: vec![
-                    Kind::Def,
-                    Kind::Symbol,
-                    Kind::LeftParen,
-                    Kind::RightParen,
-                    Kind::Colon,
-                    Kind::Indent,
-                    Kind::Symbol,
-                    Kind::Equal,
-                    Kind::Int,
-                    Kind::Indent,
-                    Kind::Symbol,
-                    Kind::Equal,
-                    Kind::Int,
-                    Kind::Indent,
-                    Kind::Symbol,
-                    Kind::Plus,
-                    Kind::Symbol,
-                ],
-                symbols: strings(["start", "x", "y", "x", "y"]),
-                ints: strings(["5", "20"]),
-                indents: vec![4, 4, 4],
-            }]
-        }
-    )
+        token_string(&tokens),
+        r#"
+Def
+Symbol start
+LeftParen
+RightParen
+Colon
+Indent 4
+Symbol x
+Equal
+Int 5
+Indent 4
+Symbol y
+Equal
+Int 20
+Indent 4
+Symbol x
+Plus
+Symbol y
+"#
+    );
 }
 
 #[test]
